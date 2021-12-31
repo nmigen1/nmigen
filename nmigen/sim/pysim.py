amaranth-lang/amaranth

sim: represent time internally as 1ps units
Using floats to represent simulation time internally isn't ideal
instead use 1ps internal units while continuing to use a floating
point based interface for compatibility.

Fixes #535.

amaranth/sim/pysim.py
@@ -49,10 +49,6 @@ class _NameExtractor:
 
 class _VCDWriter:
     @staticmethod
-    def timestamp_to_vcd(timestamp):
-        return timestamp * (10 ** 10) # 1/(100 ps)
-
-    @staticmethod
     def decode_to_vcd(signal, value):
         return signal.decoder(value).expandtabs().replace(" ", "_")
 
@@ -65,7 +61,7 @@ class _VCDWriter:
         self.vcd_vars = SignalDict()
         self.vcd_file = vcd_file
         self.vcd_writer = vcd_file and VCDWriter(self.vcd_file,
-            timescale="100 ps", comment="Generated by Amaranth")
+            timescale="1 ps", comment="Generated by Amaranth")
 
         self.gtkw_names = SignalDict()
         self.gtkw_file = gtkw_file
@@ -127,16 +123,15 @@ class _VCDWriter:
         if vcd_var is None:
             return
 
-        vcd_timestamp = self.timestamp_to_vcd(timestamp)
         if signal.decoder:
             var_value = self.decode_to_vcd(signal, value)
         else:
             var_value = value
-        self.vcd_writer.change(vcd_var, vcd_timestamp, var_value)
+        self.vcd_writer.change(vcd_var, timestamp, var_value)
 
     def close(self, timestamp):
         if self.vcd_writer is not None:
-            self.vcd_writer.close(self.timestamp_to_vcd(timestamp))
+            self.vcd_writer.close(timestamp)
 
         if self.gtkw_save is not None:
             self.gtkw_save.dumpfile(self.vcd_file.name)
@@ -158,11 +153,11 @@ class _VCDWriter:
 
 class _Timeline:
     def __init__(self):
-        self.now = 0.0
+        self.now = 0
         self.deadlines = dict()
 
     def reset(self):
-        self.now = 0.0
+        self.now = 0
         self.deadlines.clear()
 
     def at(self, run_at, process):

nmigen1/nmigen

nmigen/sim/pysim.py
@@ -49,10 +49,6 @@ class _NameExtractor:
 
 class _VCDWriter:
     @staticmethod
-    def timestamp_to_vcd(timestamp):
-        return timestamp * (10 ** 10) # 1/(100 ps)
-
-    @staticmethod
     def decode_to_vcd(signal, value):
         return signal.decoder(value).expandtabs().replace(" ", "_")
 
@@ -65,7 +61,7 @@ class _VCDWriter:
         self.vcd_vars = SignalDict()
         self.vcd_file = vcd_file
         self.vcd_writer = vcd_file and VCDWriter(self.vcd_file,
-            timescale="100 ps", comment="Generated by nMigen")
+            timescale="1 ps", comment="Generated by nMigen")
 
         self.gtkw_names = SignalDict()
         self.gtkw_file = gtkw_file
@@ -127,16 +123,15 @@ class _VCDWriter:
         if vcd_var is None:
             return
 
-        vcd_timestamp = self.timestamp_to_vcd(timestamp)
         if signal.decoder:
             var_value = self.decode_to_vcd(signal, value)
         else:
             var_value = value
-        self.vcd_writer.change(vcd_var, vcd_timestamp, var_value)
+        self.vcd_writer.change(vcd_var, timestamp, var_value)
 
     def close(self, timestamp):
         if self.vcd_writer is not None:
-            self.vcd_writer.close(self.timestamp_to_vcd(timestamp))
+            self.vcd_writer.close(timestamp)
 
         if self.gtkw_save is not None:
             self.gtkw_save.dumpfile(self.vcd_file.name)
@@ -158,11 +153,11 @@ class _VCDWriter:
 
 class _Timeline:
     def __init__(self):
-        self.now = 0.0
+        self.now = 0
         self.deadlines = dict()
 
     def reset(self):
-        self.now = 0.0
+        self.now = 0
         self.deadlines.clear()
 
     def at(self, run_at, process):