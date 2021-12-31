fad4ced2e6b4db9a824502d60266e571926b648c
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-12-31 15:09:50 +0000

    remove exclude tests in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     extras_require={
         "remote-build": ["paramiko~=2.7"],
     },
-    packages=find_packages(exclude=["*.test*"]),
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "nmigen-rpc = nmigen.rpc:main",
